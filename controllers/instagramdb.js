47120b88000df5f3d26d7095d23d986f61618a9d
Roberto Acosta <robetorobatox186@gmail.com>
2021-03-27 22:07:04 -0400

    commit4

--- controllers/instagramdb.js
+++ controllers/instagramdb.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 const client = new Client("postgres: // eiqagrbqkqxyru : 5cb4b70c4d20ef1d142ea566a7c05761bb83a0de6ba8140b45ce43bb78821e2b @ ec2-52-44-31-100.compute-1.amazonaws.com : 5432 / d1t0vrpf66i4h6")
 
-const getusers = async (req, res) => {
+const getusers =(req, res) => {
     
    
         console.log('pasa por aqui 1')
