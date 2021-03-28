abefb49900cb668c03f87145e5884b0a33149aed
Roberto Acosta <robetorobatox186@gmail.com>
2021-03-28 12:08:58 -0400

    commit7

--- controllers/instagramdb.js
+++ controllers/instagramdb.js
@@ -26,7 +26,7 @@ const getusers = async(req, res) => {
                          console.log(resp);
                       console.log(response.rows)               
                
-                  if(!!!resp){
+                  if(response.rowCount >0){
                   if(bcrypt.compareSync(contrasena,resp.password)){
                       res.send({status:200,body:response.rows})
                   }else {
