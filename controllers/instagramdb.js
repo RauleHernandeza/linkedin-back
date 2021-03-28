6367a4021420dddd58922c9ba9000fff59041145
Roberto Acosta <robetorobatox186@gmail.com>
2021-03-28 11:46:48 -0400

    commit7

--- controllers/instagramdb.js
+++ controllers/instagramdb.js
@@ -20,7 +20,7 @@ const getusers = async(req, res) => {
         console.log(req.body);
        client.connect();
       await client.query('SELECT * FROM user_1 where email=$1',[req.body.email]).then(response=>{
-                       let resp=response.rows[0];
+                       let resp=response.rows;
                          console.log(resp);
                        
                   client.end();
